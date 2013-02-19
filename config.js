44c394ced829e7d6e61bc281522d1332f12d64b2
Qawelesizwe <qawemlilo@gmail.com>
2013-02-19 09:16:26 +0200

    Moved all configuration to config.js so that users do not have to make changes on the main app file

--- config.js
+++ config.js
@@ -1,4 +1,15 @@
+/*
+   This application reqires that you create an app specific password on your gmail account.
+   Save you email address in GmailAuth.email and your app specific password in GmailAuth.password
+   
+   The sendToAddress requires the email address to send all error messages to.
+*/
+
 module.exports = {
-    email: 'your-handle@gmail.com',
-    password: 'xxxxxxxxxxxx'
+    GmailAuth: {
+        email: 'your-handle@gmail.com',
+        password: 'xxxxxxxxxxxx'
+    },
+    
+    sendToAddress: 'email-address-to-send-downtime-messages-to'
 };
