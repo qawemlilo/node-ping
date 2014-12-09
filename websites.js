3b023339cb6c26e0293a08e3c2d5bd1b039cafbe
Qawelesizwe <qawemlilo@gmail.com>
2014-12-09 21:32:42 +0200

    ...

--- websites.js
+++ websites.js
@@ -12,11 +12,11 @@ module.exports = [
         url: 'http://node-ping.herokuapp.com',
         timeout: 30
     },
-    
+    /*
     {
         url: 'http://nodetube.ragingflame.co.za',
         timeout: 25
-    },
+    },*/
     
     {
         url: 'http://nodeza.co.za',
