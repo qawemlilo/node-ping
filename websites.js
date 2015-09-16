ff7c166684eb8ac2e8a1021e0bee019da359ae34
Qawelesizwe <qawemlilo@gmail.com>
2015-09-16 08:19:33 +0200

    ..

--- websites.js
+++ websites.js
@@ -5,34 +5,34 @@
 module.exports = [
     {
         url: 'http://noble.herokuapp.com',
-        timeout: 10
+        timeout: 15
     },
     
     {
         url: 'http://node-ping.herokuapp.com',
-        timeout: 10
+        timeout: 15
     },
     {
         url: 'http://video-download.herokuapp.com/',
-        timeout: 10
+        timeout: 15
     },
     
     {
         url: 'http://nodeza.co.za',
-        timeout: 10
+        timeout: 15
     },
     
     {
         url: 'http://www.ragingflame.co.za',
-        timeout: 10
+        timeout: 15
     },
     {
         url: 'http://blog.ragingflame.co.za',
-        timeout: 10
+        timeout: 15
     },
     {
         url: 'https://gatemaster.herokuapp.com',
-        timeout: 10
+        timeout: 15
     }
     
 ];
